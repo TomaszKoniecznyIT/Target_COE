77f1ebf767907a16fc6674a2b328dd3dda9d57e6
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-07-30 13:32:12 +0100

    AddTarget and css

--- src/components/Targets/AddTarget.js
+++ src/components/Targets/AddTarget.js
@@ -1,14 +1,20 @@
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 
+import classes from "./AddTarget.module.css";
+
 const AddTarget = () => {
   const addTargetHandler = () => {
     console.log("new target");
   };
 
   return (
-    <Card>
-      <Button onClick={addTargetHandler}>Add New Target</Button>
+    <Card className={classes.input}>
+      <form>
+        <label htmlFor="target">Weekly target</label>
+        <input id="target" type="number"></input>
+        <Button onClick={addTargetHandler}>Add New Target</Button>
+      </form>
     </Card>
   );
 };
