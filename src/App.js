f7207c6f276b13f4cdb3e16f6cc71ca6dae26d0e
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-07-31 11:32:34 +0100

    adding AddDays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import AddDay from "./components/Days/AddDay";
 import AddTarget from "./components/Targets/AddTarget";
+import AddDays from "./components/Days/AddDays";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <AddTarget />
+      <AddDays />
     </div>
   );
 }
---
+++ src/components/Days/AddDays.js
@@ -0,0 +1,15 @@
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+
+const AddDays = () => {
+  return (
+    <Card>
+      <form>
+        <label>Results of individual days</label>
+      </form>
+      <Button type="submit">Upgrade days</Button>
+    </Card>
+  );
+};
+
+export default AddDays;
