8d966e00b191ce9df5704240ef49e23618393fcd
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-08-02 20:16:57 +0100

    adding required level of sales levels

--- src/components/Days/DaySaleToTarget.js
+++ src/components/Days/DaySaleToTarget.js
@@ -1,11 +1,37 @@
 import Card from "../UI/Card";
 
+const formatter = new Intl.NumberFormat("en-UK", {
+  style: "currency",
+  currency: "GBP",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
 const DaySakeToTarget = (props) => {
   return (
     <Card>
       <h2>We still have to do: {props.target100 - props.suma}</h2>
       <h2>
-        Daily target: {(props.target100 - props.suma) / props.howManyDays0}
+        Daily target 100%:{" "}
+        {formatter.format((props.target100 - props.suma) / props.howManyDays0)}
+      </h2>
+      <h2>
+        Daily target 110%:{" "}
+        {formatter.format(
+          (props.target100 * 1.1 - props.suma) / props.howManyDays0
+        )}
+      </h2>
+      <h2>
+        Daily target 115%:{" "}
+        {formatter.format(
+          (props.target100 * 1.15 - props.suma) / props.howManyDays0
+        )}
+      </h2>
+      <h2>
+        Daily target 120%:{" "}
+        {formatter.format(
+          (props.target100 * 1.2 - props.suma) / props.howManyDays0
+        )}
       </h2>
     </Card>
   );
